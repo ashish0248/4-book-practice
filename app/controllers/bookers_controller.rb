51db734cc89cbb54515025c09d5739adcf9c75ff
vagrant <vagrant@localhost.localdomain>
2020-04-05 05:11:00 +0000

    make index-page

--- app/controllers/bookers_controller.rb
+++ app/controllers/bookers_controller.rb
@@ -1,13 +1,29 @@
 class BookersController < ApplicationController
   def index
+  	@bookers = Booker.all
+  	@booker = Booker.new
   end
 
   def top
   end
 
   def show
+  	@booker = Booker.find(params[:id])
   end
 
   def edit
+  end
+
+  def create
+  	booker = Booker.new(booker_params)
+  	booker.save
+  	redirect_to booker_path(booker.id)
+  end
+
+
+
+  private
+  def booker_params
+  	params.require(:booker).permit(:title, :body)
   end
 end
